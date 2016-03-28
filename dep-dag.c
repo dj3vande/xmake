2831d7c2b295f21444148585e9475a917a09f591
Dave Vandervies <dj3vande@terse.ca>
2016-03-28 19:40:48 -0400

    Dependency tracking.
    Hope it works.

---
+++ dep-dag.c
@@ -0,0 +1,92 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#include <err.h>
+
+#include "dep-dag.h"
+
+struct dep_list dag_by_status[BUILD_NUM_STATUS];
+
+void dep_vec_add(struct dep_vec *v, struct dep_node *n)
+{
+	if (v->num >= v->max)
+	{
+		void *t;
+		size_t newmax = v->max * 2;
+		if (newmax == 0) newmax = 1;
+		t = realloc(v->nodes, newmax * sizeof *(v->nodes));
+		if (t == NULL)
+			err(EXIT_FAILURE, "dep_vec_add: realloc");
+		v->max = newmax;
+		v->nodes = t;
+	}
+	v->nodes[v->num++] = n;
+}
+
+void dep_list_unlink(struct dep_list *l, struct dep_node *n)
+{
+	assert(n->container == l);
+
+	if (n->prev == NULL)
+		l->head = n->next;
+	else
+		n->prev->next = n->next;
+	if (n->next == NULL)
+		l->tail = n->prev;
+	else
+		n->next->prev = n->prev;
+
+	n->prev = n->next = NULL;
+	n->container = NULL;
+}
+
+void dep_list_link(struct dep_list *l, struct dep_node *n)
+{
+	if (n->container == l)
+		return;
+	if (n->container != NULL)
+		dep_list_unlink(n->container, n);
+
+	assert(n->next == NULL && n->prev == NULL);
+
+	if (l->head == NULL)
+		l->head = l->tail = n;
+	else
+	{
+		n->prev = l->tail;
+		l->tail = n;
+	}
+	n->container = l;
+}
+
+void dag_set_status(struct dep_node *node, enum build_status stat)
+{
+	if (stat == BUILD_BLOCKED && node->dependencies.num == 0)
+		stat = BUILD_READY;
+	node->status = stat;
+	dep_list_link(&dag_by_status[stat], node);
+	if (stat == BUILD_DONE)
+	{
+		/* Rescan dependents to see if we've unblocked them */
+		size_t i;
+		for (i = 0; i < node->dependents.num; i++)
+		{
+			struct dep_node *n = node->dependents.nodes[i];
+			size_t j;
+			enum build_status s = BUILD_READY;
+			for (j = 0; j < n->dependencies.num; j++)
+				if (n->dependencies.nodes[j]->status != BUILD_DONE)
+					s = BUILD_BLOCKED;
+			dag_set_status(n, s);	/* will not recurse */
+		}
+	}
+}
+
+void dag_add_dependency(struct dep_node *node, struct dep_node *dependency)
+{
+	dep_vec_add(&node->dependencies, dependency);
+	dep_vec_add(&dependency->dependents, node);
+
+	if (dependency->status != BUILD_READY)
+		dag_set_status(node, BUILD_BLOCKED);
+}
---
+++ dep-dag.h
@@ -0,0 +1,70 @@
+#ifndef H_DEP_DAG
+#define H_DEP_DAG
+
+#include <stddef.h>
+
+enum build_status
+{
+	BUILD_BLOCKED,	/* dependency is not done */
+	BUILD_READY,	/* all dependencies are done */
+	BUILD_RUNNING,	/* currently working on it */
+	BUILD_DONE,	/* completely finished */
+	BUILD_NUM_STATUS
+};
+
+/*
+ * Every dep_node can be in at most one dep_list (used for keeping track of
+ * which nodes are in which state).  A dep_node knows which dep_list it's in
+ * (so a dep_node pointer has all the information we need to move that node
+ * from one list to another).
+ * dep_vec is a many-to-many container for dep_node, but without the
+ * traceability.
+ */
+
+struct dep_node;
+
+struct dep_vec
+{
+	struct dep_node **nodes;
+	size_t num, max;
+};
+
+struct dep_list
+{
+	struct dep_node *head, *tail;
+};
+
+struct dep_node
+{
+	char *name;	/* Nominally filename */
+	enum build_status status;
+
+	char *command;
+	struct {
+		int pid;	/* 0 if not running */
+		int pipefd;	/* to read output from, -1 if absent */
+		char *output;	/* NULL if not run */
+	} build_state;
+
+	struct dep_vec dependencies;
+	struct dep_vec dependents;
+	struct dep_list *container;
+	struct dep_node *next, *prev;
+};
+
+extern struct dep_list dag_by_status[BUILD_NUM_STATUS];
+
+/* Add an entry to a dep_vec, allocating more memory if necessary */
+void dep_vec_add(struct dep_vec *v, struct dep_node *n);
+
+/* dep_list bookkeeping */
+void dep_list_unlink(struct dep_list *l, struct dep_node *n);
+void dep_list_link(struct dep_list *l, struct dep_node *n);
+
+/* Set the current build status */
+void dag_set_status(struct dep_node *node, enum build_status stat);
+
+/* Add a dependency relation and update all bookkeeping */
+void dag_add_dependency(struct dep_node *node, struct dep_node *dependency);
+
+#endif	/* H_DEP_DAG #include guard */
